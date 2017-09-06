055c8272dedc646c3f47c1c5070840347f37957d
林昀龍 <cloud0822@gmail.com>
2017-09-06 22:57:30 +0800

    init this project

---
+++ angularjs-utils/utils.js
@@ -0,0 +1,13 @@
+﻿app.service("utils", function () {
+    this.isDate = function () {
+        var isDate = _.isDate(s),
+            result;
+
+        if (!isDate) {
+            result = new Date(s);        
+            isDate = (result !== "Invalid Date" && !isNaN(result));        
+        }
+
+        return isDate;    
+    };
+});
